466ab053cc26f27d0660e624d17b3de85821f7d6
hamzanaseem792 <hamzagondal792@gmail.com>
2024-06-06 15:28:13 +0200

    I made few changes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 let changer = document.querySelector('#colorButton');
+let display = document.querySelector('.color_hexavalue')
 
 const generateRandom = () => {
-    const colors = ['red', 'green', 'blue', 'yellow', 'gray', 'brown', 'blue'];
+    const colors = ['#ff0000', '#00ff00', '#0000ff', '#ffff00', '#ff00ff', '#00ffff', '#000000', '#ffffff'];
     const randomIndex = Math.floor(Math.random() * colors.length);
     return colors[randomIndex];
 }
@@ -9,4 +10,11 @@ const generateRandom = () => {
 changer.addEventListener('click', () => {
     const chosenColor = generateRandom();
     document.body.style.backgroundColor = chosenColor;
+    display.textContent = `The Hexadecimal value of this color is  ${chosenColor.toUpperCase()}`
+    if(chosenColor === '#000000'){
+        document.body.style.color = 'white'
+    }
+    else  if(chosenColor === '#ffffff'){
+        document.body.style.color = 'black'
+    }
 });
